8422228e3b915867a1470de132ebe59a35063428
end-2 <sejongchoi.400@gmail.com>
2023-05-30 23:37:21 +0900

    Improving readability

--- epsilon.go
+++ epsilon.go
@@ -17,8 +17,8 @@ const (
 	ParentsIDMask       = uint32(1)<<ParentsIDBits - 1
 	SequenceNumberMask  = uint32(1)<<SequenceNumberBits - 1
 
-	MaxPID = uint32((2 << 9) - 1)
-	MaxSeq = uint32((2 << 10) - 1)
+	MaxPID = uint32((2 << ParentsIDBits) - 1)
+	MaxSeq = uint32((2 << SequenceNumberBits) - 1)
 )
 
 var (
@@ -28,7 +28,7 @@ var (
 
 // Epsilon is a 64-bit ID(composed of timestamp, pid, seq) generator.
 type Epsilon struct {
-	zeroTime time.Time
+	baseTime time.Time
 	pid      uint64
 	seq      uint32
 	prevTime uint64
@@ -36,13 +36,13 @@ type Epsilon struct {
 }
 
 // New function create Epsilon based on base time and pid.
-func New(zeroTime time.Time, pid uint32) (*Epsilon, error) {
+func New(baseTime time.Time, pid uint32) (*Epsilon, error) {
 	if pid > MaxPID {
 		return nil, ErrPIDExceed
 	}
 
 	return &Epsilon{
-		zeroTime: zeroTime,
+		baseTime: baseTime,
 		pid:      uint64(pid&ParentsIDMask) << SequenceNumberBits,
 		seq:      uint32(0),
 		prevTime: uint64(0),
@@ -52,7 +52,7 @@ func New(zeroTime time.Time, pid uint32) (*Epsilon, error) {
 
 // Now function return epsilon current time.
 func (e *Epsilon) Now() uint64 {
-	return uint64(time.Since(e.zeroTime).Nanoseconds())
+	return uint64(time.Since(e.baseTime).Nanoseconds())
 }
 
 // Next function return id, if sequential number is full then return ErrSeqNumOverflow.
@@ -61,14 +61,16 @@ func (e *Epsilon) Next() (uint64, error) {
 	defer e.mutex.Unlock()
 
 	timestamp := (e.Now() & TimestampMask) << TimestampShiftBits
+
 	if e.prevTime < timestamp {
 		e.prevTime = timestamp
 		e.seq = 0
-		return timestamp + e.pid + uint64(0), nil
-	} else if e.seq > MaxSeq {
-		return 0, ErrSeqNumOverflow
 	} else {
+		if e.seq > MaxSeq {
+			return 0, ErrSeqNumOverflow
+		}
 		e.seq++
-		return timestamp + e.pid + uint64(e.seq&SequenceNumberMask), nil
 	}
+
+	return timestamp | e.pid | uint64(e.seq&SequenceNumberMask), nil
 }
